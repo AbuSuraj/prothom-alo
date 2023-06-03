818583f524307e2cd9fb16f976e92650f936c7ae
Abu Suraj <abusurajbd@gmail.com>
2023-06-03 21:29:30 +0600

    add active class in button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,21 @@ export class AppComponent implements OnInit {
   news: any;
   title = 'prothom-alo';
   clickedDate: any;
+  isValue: string ='1';
+
   constructor(private newsService: NewsServiceService){}
+  
   ngOnInit(): void {  
     this.newsService.news().subscribe(result => {
       this.news = result;
       console.log(result);
     })
   }
+  
   headline(date:any){
     this.clickedDate = date;
+    this.isValue = date;
   }
+
+  // toggle() { this.isValue = num; }
 }
